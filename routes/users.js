e5544eb2e2a89ffc83501ba1ed8e9f472d194e17
Yurii Maystrenko <yura1994hd@gmail.com>
2019-03-11 20:51:34 +0200

    init master

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+function randomInteger(min, max) {
+    var rand = min - 0.5 + Math.random() * (max - min + 1);
+    rand = Math.round(rand);
+    return rand;
+}
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/count_online', function(req, res, next) {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ count_online: randomInteger(50, 1000) }));
 });
 
 module.exports = router;
